ef59d7021bfb24b2e137eeca8c228a596a93ff0f
Longtian <wyvernnot@gmail.com>
2014-07-22 09:27:26 +0800

    grunt watch and grunt connect

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,9 +4,32 @@
 
 module.exports=function(grunt){
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.initConfig({
        jshint: {
         all: ['Gruntfile.js', '*.js']
+      },
+      connect:{
+          server:{
+              options:{
+                port:80,
+                base:"app",
+                keepalive:false,
+                open:{
+                        target:"http://localhost/index.html",
+                        appName:"Chrome"
+                    }
+              }
+          },
+      },
+      watch:{
+          files:["app/**/*.js","app/**/*.html","app/**/*.css"],
+           options: {
+                livereload: true
+            },
       }
     });
+    
+    grunt.registerTask('default',["connect","watch"]);
 };
